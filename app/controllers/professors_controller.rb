4fb5e9d975fde15b8d3346fa8dde2494a9a7bad3
isabellagh <isabellagh88@gmail.com>
2020-09-17 21:56:45 -0400

    link_to professor

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,5 +1,15 @@
 class ProfessorsController < ApplicationController
 
   def index
+    @professors = Professor.all
   end
+
+  def create
+    @professor = Professor.create (params)
+  end 
+
+  def show 
+    @professor = Professor.new (params)
+  end 
+
 end
